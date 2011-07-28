888e04a6a93bae3c773bd31fc0b658bffab9117e
Dmitrii Samoilov <e2718281828@ya.ru>
2011-07-28 23:49:15 +0300

    updated atomicity_test.rb

--- test/atomicity_test.rb
+++ test/atomicity_test.rb
@@ -1,25 +1,43 @@
 require File.dirname(File.expand_path(__FILE__)) + '/test_helper.rb'
 
 class Article < RedisOrm::Base
+  use_uuid_as_id
+  
   property :title, String
   property :karma, Integer
 end
 
 describe "check atomicity" do
   it "should properly increment property's value" do
-    @article = Article.new :title => "Simple test atomicity with multiple threads", :karma => 1
-    @article.save
+    article = Article.new :title => "Simple test atomicity with multiple threads", :karma => 1
+    article.save
     
-    @threads = []
+    threads = []
     
     50.times do |i|
-      @threads << Thread.new(i) do
-        @article.update_attribute :karma, (@article.karma + 1)
+      threads << Thread.new(i) do
+        article.update_attribute :karma, (article.karma + 1)
       end
     end
     
-    @threads.each{|thread| thread.join}
+    threads.each{|thread| thread.join}
     
     Article.first.karma.should == 51
+  end
+
+  it "should properly increment/decrement property's value" do
+    article = Article.create :title => "article #1", :karma => 10
+    threads = []
+    
+    10.times do
+      threads << Thread.new{ article.update_attribute(:karma, (article.karma + 2)) }
+    end
+    
+    15.times do
+      threads << Thread.new{ article.update_attribute(:karma, (article.karma - 1)) }
+    end
+    
+    threads.each{|thread| thread.join}
+    article.karma.should == 15
   end
 end
