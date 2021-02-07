686b742a6e6d779038d0c34fcbe28930004c2058
Dmytro Samoilov <dmytro.samoilov@simplybusiness.co.uk>
2021-02-07 22:33:21 +0200

    Updated exceptions spec

--- spec/models/exceptions_spec.rb
+++ spec/models/exceptions_spec.rb
@@ -2,46 +2,45 @@ require 'spec_helper.rb'
 
 describe "exceptions test" do
   it "should raise an exception if association is provided with improper class" do
-    User.count.should == 0
+    expect(User.count).to eq(0)
 
     user = User.new :name => "german", :age => 26
     user.save
 
-    user.should be
-    user.name.should == "german"
-    User.count.should == 1
+    expect(user).to be
+    expect(user.name).to eq("german")
+    expect(User.count).to eq(1)
 
-    lambda{ User.find :all, :conditions => {:gender => true} }.should raise_error
-    User.find(:all, :conditions => {:age => 26}).size.should == 1
-    lambda{ User.find :all, :conditions => {:name => "german", :age => 26} }.should raise_error
+    expect(lambda{ User.find :all, :conditions => {:gender => true} }).to raise_error(RedisOrm::NotIndexFound)
+    expect(User.find(:all, :conditions => {:age => 26}).size).to eq(1)
+    expect(lambda{ User.find :all, :conditions => {:name => "german", :age => 26} }).to raise_error(RedisOrm::NotIndexFound)
     
     jigsaw = Jigsaw.new
     jigsaw.title = "123"
     jigsaw.save
 
-    # RedisOrm::TypeMismatchError
-    lambda { user.profile = jigsaw }.should raise_error
+    expect(lambda { user.profile = jigsaw }).to raise_error(RedisOrm::TypeMismatchError)
   end
   
   it "should raise an exception if there is no such record in the storage" do
-    User.find(12).should == nil
-    lambda{ User.find! 12 }.should raise_error(RedisOrm::RecordNotFound)
+    expect(User.find(12)).to be_nil
+    expect(lambda{ User.find! 12 }).to raise_error(RedisOrm::RecordNotFound)
   end
 
   it "should throw an exception if there was an error while creating object with #create! method" do
     jigsaw = Jigsaw.create :title => "jigsaw"
-    lambda { User.create!(:name => "John", :age => 44, :profile => jigsaw) }.should raise_error(RedisOrm::TypeMismatchError)
+    expect(lambda { User.create!(:name => "John", :age => 44, :profile => jigsaw) }).to raise_error(RedisOrm::TypeMismatchError)
   end
 
   it "should throw an exception if wrong format of the default value is specified for Array/Hash property" do
     a = ArticleWithComments.new :title => "Article #1", :rates => [1,2,3,4,5]
-    lambda {
+    expect(lambda {
       a.save
-    }.should raise_error(RedisOrm::TypeMismatchError)
+    }).to raise_error(RedisOrm::TypeMismatchError)
     
     a = ArticleWithComments.new :title => "Article #1", :comments => 12
-    lambda {
+    expect(lambda {
       a.save
-    }.should raise_error(RedisOrm::TypeMismatchError)
+    }).to raise_error(RedisOrm::TypeMismatchError)
   end
 end
